41e040a026a95e47bfffb8f61707c6006a5484aa
Venrum <21.12.2012@mail.ru>
2019-07-07 10:47:02 +0300

    Add files via upload
    Added hex_read file for making another hex command to control robot from different devices, using HEX code.

---
+++ hex_read.ino
@@ -0,0 +1,24 @@
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
+
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Запускаем IR приемник
+}
+
+/*получаем данные с источника*/
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+}
